4c2dc0a7f37d5d66371f6ca385828569b7f569b0
Jason <59513650+jasonsk86@users.noreply.github.com>
2020-01-14 17:56:55 +0000

    initial project upload

---
+++ 2020-wk2-passwords/passwords.R
@@ -0,0 +1,11 @@
+
+
+# Load libraries ---------------------------------------------------------
+library(tidyverse)
+
+
+# Load data ---------------------------------------------------------------
+
+passwords <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')
+
+
