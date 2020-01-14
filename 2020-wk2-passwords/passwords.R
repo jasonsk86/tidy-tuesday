2fd83cbbee60e3072f3b1149e01c344c8db7dc2c
Jason <59513650+jasonsk86@users.noreply.github.com>
2020-01-14 20:32:59 +0000

    updated visualisation for tidy tuesday

--- 2020-wk2-passwords/passwords.R
+++ 2020-wk2-passwords/passwords.R
@@ -1,6 +1,7 @@
 
 
 # Load libraries ---------------------------------------------------------
+
 library(tidyverse)
 library(tidylog)
 library(gghighlight)
@@ -13,7 +14,6 @@ library(showtext)
 passwords_orig <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')
 passwords <- passwords_orig
 
-passwords
 
 # Data prep ---------------------------------------------------------------
 
@@ -33,7 +33,6 @@ passwords <- passwords %>%
     online_crack_days = online_crack_seconds / 86400
   )
 
-passwords  
 
 fluffy_worst <- passwords %>% 
   filter(category == "fluffy") %>% 
@@ -56,27 +55,30 @@ passwords %>%
   geom_bar(stat = "identity", fill = "firebrick",
            aes(x = reorder(category, online_time), y = online_time)) +
   gghighlight(category == "fluffy") +
-  annotate(geom = "label", x = "fluffy", y = 1000, 
+  annotate(geom = "text", x = "fluffy", y = 650, 
            label = glue("Worst offenders include {fluffy_worst[1]}, {fluffy_worst[2]} and {fluffy_worst[3]}"),
-           fill = "firebrick", color = "white", alpha = 0.8, size = 8) +
+           color = "firebrick", size = 5) +
   labs(title = "Stop Using 'Fluffy' Passwords If You Don't Want to Be Hacked",
        subtitle = "Average Time to Crack Password Online by Password Categorisation",
-       y = "Average Time to Crack Online (days)",
+       y = "Average Time to Crack Password Online (days)",
        caption = "Plot: Jason Skelton | Data: Information is Beautiful") +
-  scale_y_continuous(breaks = seq(0, 2500, 250)) +
+  scale_y_continuous(breaks = seq(0, 2500, 250), expand = c(0,0)) +
   coord_flip() +
   theme_minimal() +
   theme(
     text = element_text(family = "lato"),
-    plot.title = element_text(size = 20, face = "bold", margin = margin(0,0,15,0)), 
-    plot.subtitle = element_text(size = 16),
-    plot.caption = element_text(size = 12, color = "darkgray"),
+    plot.title = element_text(size = 26, face = "bold", margin = margin(0,0,10,0)), 
+    plot.subtitle = element_text(size = 18, margin = margin(0,0,10,0)),
+    plot.caption = element_text(size = 13, color = "#706968", face = "bold"),
     panel.grid.minor = element_blank(),
     panel.grid.major = element_blank(),
     axis.text = element_text(size = 16),
     axis.title.y = element_blank(),
-    axis.title.x = element_text(margin = margin(15,0,0,0), face = "bold", size = 12)
+    axis.title.x = element_text(margin = margin(15,0,0,0), face = "bold", size = 16),
+    axis.line = element_line(color = "darkgray"),
+    axis.ticks = element_line(color = "darkgray")
   )
 
-ggsave('passwords.png', height = 10, width = 10, dpi = "retina")
+
+ggsave('passwords.png', dpi = "retina")
             
